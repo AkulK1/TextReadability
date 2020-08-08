5a5243f0ee8cfc06679aeb31b7124e7cca7153c4
AkulK1 <64177312+AkulK1@users.noreply.github.com>
2020-08-08 19:14:02 -0400

    Update DataCalculation.py

--- DataCalculation.py
+++ DataCalculation.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug  8 14:43:49 2020
-
-@author: kesar
-"""
-
-
 import pandas as pd
 
 df = pd.read_csv( "BookDataForMLTextComplexityProject.csv" )
@@ -16,3 +8,19 @@ print(df['difficulty'].value_counts())
 
 
 print(df[['text', 'difficulty']])
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+#ff = open( 'TextData/b168_DreaminginCuban.txt', 'r', encoding="utf8" )
+
+
+
+def get_nlp( str ):
+    ff = open( 'TextData/' + str, 'r', encoding = 'utf8', errors = 'ignore' )
+    print(str)
+    return nlp(ff.read())
+
+
+df['docs'] = df['text'].apply( get_nlp )
