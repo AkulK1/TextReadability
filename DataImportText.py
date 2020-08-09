104eb4bd4f6f37c7d541ddad7ee0a3c8c42326b6
AkulK1 <64177312+AkulK1@users.noreply.github.com>
2020-08-09 14:30:45 -0400

    Update DataImportText.py

--- DataImportText.py
+++ DataImportText.py
@@ -9,9 +9,7 @@ print(df['difficulty'].value_counts())
 
 print(df[['text', 'difficulty']])
 
-import spacy
 
-nlp = spacy.load("en_core_web_sm")
 
 #ff = open( 'TextData/b168_DreaminginCuban.txt', 'r', encoding="utf8" )
 
@@ -19,8 +17,8 @@ nlp = spacy.load("en_core_web_sm")
 
 def get_nlp( str ):
     ff = open( 'TextData/' + str, 'r', encoding = 'utf8', errors = 'ignore' )
-    print(str)
-    return nlp(ff.read())
+    print( str )
+    return ff.read()
 
 
 df['docs'] = df['text'].apply( get_nlp )
