c23bb64024bcba1291622053e9a29112361ab396
AkulK1 <64177312+AkulK1@users.noreply.github.com>
2020-08-10 12:56:42 -0400

    combined calculations
    Combined sentence calc and syllable calc

---
+++ FeatureCalculation.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  9 14:04:12 2020
+
+@author: kesar
+"""
+
+
+import pandas as pd
+import spacy
+from spacy_syllables import SpacySyllables
+
+nlp = spacy.load("en_core_web_sm")
+syllables = SpacySyllables(nlp)
+nlp.add_pipe(syllables, after="tagger")
+
+
+df = pd.read_csv( "DataWithTexts.csv" , index_col = 0)
+
+df.head()
+print(df.iloc[-1])
+
+df['sentences'] = df['docs'].apply( lambda x: len(list(nlp(x).sents)))
+
+doc1 =nlp ("Hey let's see if I can make this sentence work properly, huh?")
+for token in doc1:
+    print( token.text, token._.syllables_count )
+    
+def spw (doc):
+    ws =0
+    syls =0
+    for token in doc:
+        if token._.syllables_count != None:
+            ws+=1
+            syls+=token._.syllables_count
+    print( syls/ws )
+    return syls/ws
+
+df['syl_per_word'] = df['docs'].apply( lambda x: spw(nlp(x)) )
+
+
+df.plot( x = 'syl_per_word', y = 'difficulty', style = 'o' )
