e903e0609e309e370377a7a1e13253374e814e05
BryantXia22 <64176121+BryantXia22@users.noreply.github.com>
2020-09-01 17:26:31 -0400

    added /predict

--- FlaskAPI/req_attempt.py
+++ FlaskAPI/req_attempt.py
@@ -7,7 +7,7 @@ This snipet is from Ken Jee's github'
 import requests 
 from data_input import dt_in
 
-URL = 'https://ds-text-readability-20.herokuapp.com/'
+URL = 'https://ds-text-readability-20.herokuapp.com/predict'
 headers = {"Content-Type": "application/json"}
 data = {"input": dt_in}
 
