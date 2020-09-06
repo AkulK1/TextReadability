7f61e59f265f8d472738ac20bdff22b989a827fa
kesar <AkulK1@users.noreply.github.com>
2020-09-06 14:01:23 -0400

    adding feature calc endpoint in api

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -2,6 +2,9 @@ from flask import Flask, request
 import json
 import pickle
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+import spacy
+from spacy_syllables import SpacySyllables
 
 app = Flask(__name__)
 
@@ -27,6 +30,97 @@ def predict():
     
     response = json.dumps({'response': prediction})
     return response, 200
+
+def isLetter (input):
+    for x in range (ord('a'), ord('z')+1):
+        if (input==x):
+            return True
+    for x in range (ord('A'), ord('Z')+1):
+        if (input==x):
+            return True
+    return False
+
+@app.route( '/textpred', methods = ['POST'] )
+def textpred():
+    req_text = request.get_json()['input']
+    feature_list = []
+    
+    
+    #calculating features
+    #features are ['syl_per_word', 'ws_per_sents', 'monosyls',  'flesch_score', 'ari_score', 'dale_score', 'smog','gunning_fog', 'cli', 'linsear', 'det', 'sconj', 'avg_verb_length']
+    nlp = spacy.load("en_core_web_sm")
+    syllables = SpacySyllables(nlp)
+    nlp.add_pipe(syllables, after="tagger")
+    
+    doc = nlp(req_text)
+    
+    #syl_per_word
+    ws =0
+    syls =0
+    for token in doc:
+        if token._.syllables_count != None:
+            ws+=1
+            syls+=token._.syllables_count
+    feature_list.append( syls/ws )
+    
+    #ws_per_sents    
+    sentenceCount = len(list(doc.sents))
+    wordCount = req_text.split()
+    feature_list.append( wordCount/sentenceCount )
+    
+    #monosyls
+    count_mono  = 0
+    wss = 0
+    for token in doc:
+        syl1 = token._.syllables_count
+        if  syl1 != None:
+            wss+=1
+            if syl1 == 1:
+                count_mono+=1
+    feature_list.append( count_mono/wss )
+    
+    #flesch_score
+    #df['flesch_score'] = 206.835 - 1.015*df['ws_per_sents'] -84.6*df['syl_per_word']
+    fkscore = 206.835 - 1.015*feature_list[1] - 84.6*feature_list[0]
+    feature_list.append( fkscore )
+    
+    #ari_score
+    #df['ari_score'] = 4.71*df['avg_wd_length']+0.5*df['ws_per_sents']
+    ltrCount=0
+    for word in req_text:
+        for char in word:
+            if (isLetter(ord(char))==True):
+                ltrCount+=1
+    avg_wd_len = ltrCount/wordCount
+    feature_list.append( 4.71*avg_wd_len + 0.5*feature_list[1] )
+    
+    #dale_score
+    ff = open('EasyList.txt', 'r')
+    easy_words = [i.lower() for i in ff.read().split()]
+    res = 0
+    for token in doc:
+        t1 =  token.lemma_
+        t1 = t1.lower()
+        
+        if (token._.syllables_count != None) and not ( (token.text.lower() in easy_words) or ( t1.lower() in easy_words)):
+            print(token)
+            res+=1
+    percent_easy = res/wordCount
+    feature_list.append( 0.1579*percent_easy*100 + 0.0496*feature_list[1] )
+    
+    #smog
+    
+    
+    x_in = np.array(feature_list).reshape(1, -1)
+    x_in =  StandardScaler().fit_transform( x_in )
+    # load model
+    model = load_models()
+    prediction = str(model.predict(x_in)[0])
+    print (prediction)
+    
+    response = json.dumps({'response': prediction})
+    return response, 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
