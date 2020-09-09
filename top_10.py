d079d0b1c9cfb1b2d14755d55084d2db3bfd5894
kesar <AkulK1@users.noreply.github.com>
2020-09-08 23:38:15 -0400

    Update top_10.py

--- top_10.py
+++ top_10.py
@@ -6,6 +6,7 @@ Created on Tue Sep  8 20:49:24 2020
 """
 from wordfreq import word_frequency
 from PyDictionary import PyDictionary
+from wordfreq import tokenize
 import spacy
 nlp = spacy.load("en_core_web_sm")
 
@@ -16,8 +17,11 @@ class word ():
         self.txt = txt
         self.freq = freq
 
-doc=nlp("""In ordinary language we describe by the word “planning” the complex of interrelated decisions about the allocation of our available resources. All economic activity is in this sense planning; and in any society in which many people collaborate, this planning, whoever does it, will in some measure have to be based on knowledge which, in the first instance, is not given to the planner but to somebody else, which somehow will have to be conveyed to the planner. The various ways in which the knowledge on which people base their plans is communicated to them is the crucial problem for any theory explaining the economic process, and the problem of what is the best way of utilizing knowledge initially dispersed among all the people is at least one of the main problems of economic policy—or of designing an efficient economic system.""")
 
+# doc_nlp=nlp("""In ordinary language we describe by the word “planning” the complex of interrelated decisions about the allocation of our available resources. All economic activity is in this sense planning; and in any society in which many people collaborate, this planning, whoever does it, will in some measure have to be based on knowledge which, in the first instance, is not given to the planner but to somebody else, which somehow will have to be conveyed to the planner. The various ways in which the knowledge on which people base their plans is communicated to them is the crucial problem for any theory explaining the economic process, and the problem of what is the best way of utilizing knowledge initially dispersed among all the people is at least one of the main problems of economic policy—or of designing an efficient economic system.""")
+
+raw_text = """In ordinary language we describe by the word “planning” the complex of interrelated decisions about the allocation of our available resources. All economic activity is in this sense planning; and in any society in which many people collaborate, this planning, whoever does it, will in some measure have to be based on knowledge which, in the first instance, is not given to the planner but to somebody else, which somehow will have to be conveyed to the planner. The various ways in which the knowledge on which people base their plans is communicated to them is the crucial problem for any theory explaining the economic process, and the problem of what is the best way of utilizing knowledge initially dispersed among all the people is at least one of the main problems of economic policy—or of designing an efficient economic system."""
+doc = tokenize(raw_text, 'en' )
 diff_words=[]
 all_words=[]
 
