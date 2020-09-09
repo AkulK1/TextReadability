a2d69c1463ed31d0f45f72f03549f4560b437171
BryantXia22 <64176121+BryantXia22@users.noreply.github.com>
2020-09-08 22:10:17 -0400

    Update top_10.py

--- top_10.py
+++ top_10.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep  8 21:59:23 2020
-
-@author: kuangkuang
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Tue Sep  8 20:49:24 2020
 
 @author: Bryant_Xia
