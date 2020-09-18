c569d3ed3f105d819b750547f86cee354aba2b38
BryantXia22 <64176121+BryantXia22@users.noreply.github.com>
2020-09-18 14:41:54 -0400

    Update popup.js

--- RequestExtension/popup.js
+++ RequestExtension/popup.js
@@ -1,10 +1,31 @@
-console.log( "message sending" );
+x = document.createElement("BUTTON");
+t = document.createTextNode("Click here if it's info text");
+x.appendChild(t);
+document.body.appendChild(x);
 
-var query = { active: true, currentWindow: true };
-function callback(tabs) {
-  var currentTab = tabs[0]; 
-  chrome.runtime.sendMessage( {clicked: true, curTab: currentTab}, function( response ){
-	  document.getElementById( "textdifficulty" ).innerHTML = response.res;
-  });
-}
-chrome.tabs.query(query, callback);
+var y = document.createElement("BUTTON");
+var z = document.createTextNode("Click here if it's an English Language text");
+y.appendChild(z);
+document.body.appendChild(y);
+
+x.onclick = function() {
+    var query = { active: true, currentWindow: true };
+	function callback(tabs) {
+  	var currentTab = tabs[0]; 
+  	chrome.runtime.sendMessage( {clicked: true, curTab: currentTab, info: true}, function( response ){
+	  	document.getElementById( "textdifficulty" ).innerHTML = response.res;
+  		});
+	}
+	chrome.tabs.query(query, callback);
+}	
+
+y.onclick = function() {
+    var query = { active: true, currentWindow: true };
+	function callback(tabs) {
+  	var currentTab = tabs[0]; 
+  	chrome.runtime.sendMessage( {clicked: true, curTab: currentTab, info: false}, function( response ){
+	  	document.getElementById( "textdifficulty" ).innerHTML = response.res;
+  		});
+	}
+	chrome.tabs.query(query, callback);
+}	
