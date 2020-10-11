2e584c138a7322ca86c7558a1a8fd4aaa5519373
kesar <AkulK1@users.noreply.github.com>
2020-10-11 17:37:32 -0400

    added new ui to extension

--- RequestExtension/content.js
+++
@@ -1,6 +0,0 @@
-console.log( document.documentElement.outerHTML );
-/*
-let paras = document.getElementsByTagName( 'p' );
-for( var i = 0; i < paras.length; i++ ){
-	paras[i].style['background-color'] = '#F0C';
-}*/
--- RequestExtension/popup.js
+++ RequestExtension/popup.js
@@ -1,12 +1,17 @@
-x = document.createElement("BUTTON");
+/*x = document.createElement("BUTTON");
 t = document.createTextNode("Click here if it's info text");
 x.appendChild(t);
+x.style.backgroundColor = "#2434bf"
+
 document.body.appendChild(x);
 
 var y = document.createElement("BUTTON");
 var z = document.createTextNode("Click here if it's an English Language text");
 y.appendChild(z);
+y.style.backgroundColor = "#2434bf"
 document.body.appendChild(y);
+
+
 
 x.onclick = function(){
 	getAnswer( true );
@@ -15,7 +20,7 @@ x.onclick = function(){
 
 y.onclick = function(){
 	getAnswer( false );
-}
+}*/
 /*function() {
     var query = { active: true, currentWindow: true };
 	function callback(tabs) {
@@ -28,16 +33,43 @@ y.onclick = function(){
 }	*/
 
 
+document.getElementById( "infoButton" ).onclick = function(){
+	console.log( "info button pressed" );
+	getAnswer( true );
+}
+document.getElementById( "fictionButton" ).onclick = function(){
+	console.log( "fiction button pressed" );
+	getAnswer(false);
+}
+
 function getAnswer( isInfo ){
-	var query = { active: true, currentWindow: true };
+	//console.log( "button pressed " + isInfo );
+	/*var query = { active: true, currentWindow: true };
 	function callback(tabs) {
 		document.getElementById( "textdifficulty" ).innerHTML = "Loading";
 		var currentTab = tabs[0]; 
-		chrome.runtime.sendMessage( {clicked: true, curTab: currentTab, info: Boolean(isInfo)}, function( response ){
+		//chrome.runtime.sendMessage( {clicked: true, curTab: currentTab, info: Boolean(isInfo)}, function( response ){
 			document.body.style.width = "500px";
-			var responseJSON = JSON.parse( response.res );
+			console.log( response );
+			var responseJSON = JSON.parse( response );
+			console.log( responseJSON );
 			var tdiff = responseJSON.response;
-			document.getElementById( "textdifficulty" ).innerHTML = tdiff;
+			var gradeLevel = "Sorry. URL not compatible. Try copy and pasting text at our <a href ='http://lexlearntogether.com/'> website </a>";
+			if( tdiff == 1 ){
+				gradeLevel = "Kindergarten";
+			} else if( tdiff == 2 ){
+				gradeLevel = "First to Second Grade";
+			} else if( tdiff == 3 ){
+				gradeLevel = "Third to Fourth Grade";
+			} else if( tdiff == 4 ){
+				gradeLevel = "Fifth to Sixth Grade";
+			} else if( tdiff == 5 ){
+				gradeLevel = "Seventh to Eighth Grade";
+			} else if( tdiff == 6 ){
+					gradeLevel = "High School Level or Above";
+			}
+			
+			document.getElementById( "textdifficulty" ).innerHTML = gradeLevel;
 			
 			htmlList = document.getElementById( "worddefs" );
 			console.log( responseJSON.diff_words );
@@ -51,5 +83,64 @@ function getAnswer( isInfo ){
 			
   		});
 	}
-	chrome.tabs.query(query, callback);
+	chrome.tabs.query(query, callback);*/
+	
+	var loadScreen  = document.getElementById( "loadingScreen" );
+	loadScreen.className = "loader";
+	var query = { active: true, currentWindow: true };
+	document.body.style.width = "500px";
+	document.getElementById( "textdifficulty" ).innerHTML = "";
+	document.getElementById( "worddefs" ).innerHTML = "";
+	
+	chrome.tabs.query( query, function( tabs ){
+		
+		var currentTab = tabs[0];
+		let Url = currentTab.url;
+		var xhr = new XMLHttpRequest();
+		xhr.open( "POST", "https://ds-text-readability-20.herokuapp.com/fullpredict" );
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		var category = -1;
+		if( isInfo ){
+			category = 1;
+		} else {
+			category = 0;
+		}
+		
+		xhr.send( JSON.stringify({
+			article_url: String(Url), info: category
+		}));
+		
+		xhr.onreadystatechange = function(){
+			if (this.readyState == 4 && this.status == 200){
+				loadScreen.className = "";
+				var responseJSON = JSON.parse( xhr.responseText );
+				var tdiff = responseJSON.response;
+				var gradeLevel = "Sorry. URL not compatible. Try copy and pasting text at our <a href ='http://lexlearntogether.com/'> website </a>";
+				if( tdiff == 1 ){
+					gradeLevel = "Kindergarten";
+				} else if( tdiff == 2 ){
+					gradeLevel = "First to Second Grade";
+				} else if( tdiff == 3 ){
+					gradeLevel = "Third to Fourth Grade";
+				} else if( tdiff == 4 ){
+					gradeLevel = "Fifth to Sixth Grade";
+				} else if( tdiff == 5 ){
+					gradeLevel = "Seventh to Eighth Grade";
+				} else if( tdiff == 6 ){
+						gradeLevel = "High School Level or Above";
+				}
+				
+				document.getElementById( "textdifficulty" ).innerHTML = "<b>Difficulty</b>" + ": "+ gradeLevel;
+				htmlList = document.getElementById( "worddefs" );
+				for( var propt in responseJSON.diff_words ){
+					var cur_li = document.createElement( "li" );
+					var temp_str =  propt.bold()  + ": " + responseJSON.diff_words[propt];
+					cur_li.innerHTML =  temp_str ;
+					htmlList.appendChild( cur_li );
+				}
+				
+			}
+			
+		}
+	});
 }
